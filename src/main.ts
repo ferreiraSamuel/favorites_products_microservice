73873e233ecf28ea238e02dc94adc60ce2f5d711
ferreiraSamuel <samuelfbjm@gmail.com>
2022-04-21 21:10:24 -0300

    chore(swagger): implementa swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Favorites Products MicroService')
+    .setDescription('MicroService for Favorites Products')
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
